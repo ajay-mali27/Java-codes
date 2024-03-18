bc2980f0c553161285f7d5f7efd43766ceeb2ce9
Ajay Mali <ajaymali2701@gmail.com>
2024-03-18 19:01:27 +0530

    Garbage Collector
    Codes of Garbage Collector
    
    18/03/20244

--- 1_Garbage_Collector/src/garbage/A.java
+++ 1_Garbage_Collector/src/garbage/A.java
@@ -2,4 +2,12 @@ package garbage;
 
 public class A {
 
+	void m1() {
+		
+//		System.gc()== User Defined Garbage Collector. Java Will Not Start Garbage Collector. Bcoz we have started
+		
+//		System.gc();
+		Object
+//		Just Click On the 1) System.gc() And 2) Object and Read the Documentation.
+	}
 }
