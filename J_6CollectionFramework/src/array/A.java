f73f33a2158f02093321a5856316613c437ef031
Ajay Mali <ajaymali2701@gmail.com>
2024-04-25 10:14:41 +0530

    Collection Framework -1 Array
    1 Array. how to print without cf and with cf.

---
+++ J_6CollectionFramework/src/array/A.java
@@ -0,0 +1,19 @@
+package array;
+
+public class A {
+public static void main(String[] args) {
+	
+	int a[]=new int[4];
+	int b[]=new int [8];
+	
+	a[0]= 5;
+	a[2]=8;
+	a[1]=9;
+	a[3]=46;
+	for(int i=1;i<a.length;i++)
+		
+	{
+System.out.println(a[i]);		
+	}
+}
+}
