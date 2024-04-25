33581b440c0a98cdbb3e53c193a04f6d10e18a29
Ajay Mali <ajaymali2701@gmail.com>
2024-04-25 10:15:53 +0530

    collection framework

---
+++ J_6CollectionFramework/src/CF/Y.java
@@ -0,0 +1,36 @@
+package CF;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Y {
+public static void main(String[] args) {
+	
+	ArrayList ss=new ArrayList();
+	ss.add(20);
+	ss.add(52);
+	ss.add(9);
+	ss.add(8);
+	ss.add(-4);
+	
+	
+	
+//	Sorting of array
+//	Collections.sort(ss);
+	
+	
+//	Print Minimun number
+//	System.out.println(Collections.min(ss));
+	
+//	Print Max number
+	System.out.println(Collections.max(ss));
+	
+	
+//	System.out.println(ss.get(0));
+	
+//	for(int i=0;i<ss.size();i++) {
+//		System.out.println(ss.get(i));
+//		
+//		
+//	}
+}
+}
